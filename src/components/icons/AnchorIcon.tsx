4601f5b03b02c39521ef95b1a9199b98c6071d3c
urzeye <urzeye@163.com>
2026-01-09 14:00:49 +0800

    feat: svg图标优化

--- src/components/icons/AnchorIcon.tsx
+++ src/components/icons/AnchorIcon.tsx
@@ -28,15 +28,17 @@ export const AnchorIcon: React.FC<IconProps> = ({
     className={className}
     style={{ display: "block" }}>
     {/* 顶部圆环 */}
-    <circle cx="12" cy="5" r="3" />
+    <circle cx="12" cy="5" r="2.5" />
     {/* 主干竖线 */}
-    <line x1="12" y1="8" x2="12" y2="21" />
+    <line x1="12" y1="7.5" x2="12" y2="22" />
     {/* 横杆 */}
-    <line x1="5" y1="12" x2="19" y2="12" />
-    {/* 左弯钩 */}
-    <path d="M5 12c0 4 3 7 7 9" />
-    {/* 右弯钩 */}
-    <path d="M19 12c0 4-3 7-7 9" />
+    <line x1="7" y1="10" x2="17" y2="10" />
+    {/* 底部弧线 */}
+    <path d="M4 14C4 19 8 22 12 22C16 22 20 19 20 14" />
+    {/* 箭头 - 左 */}
+    <path d="M1.5 16.5L4 14L6.5 16.5" />
+    {/* 箭头 - 右 */}
+    <path d="M17.5 16.5L20 14L22.5 16.5" />
   </svg>
 )
 
